66a2ebe859939d9d23ca04df29da693be8249b7e
CMGS <ilskdw@gmail.com>
2015-05-18 09:58:16 +0800

    bug fix

--- vlan.go
+++ vlan.go
@@ -95,18 +95,18 @@ func (self *VLanSetter) addVLan(feedKey, content, ident, containerID string) {
 		self.delVLan(vethName)
 		return
 	}
-	pid := strconv.Itoa(container.State.Pid)
 
 	// Set into container
 	ifc, _ := net.InterfaceByName(vethName)
-	if err := netlink.NetworkSetNsPid(ifc, pid); err != nil {
+	if err := netlink.NetworkSetNsPid(ifc, container.State.Pid); err != nil {
 		gore.NewCommand("LPUSH", feedKey, fmt.Sprintf("0|||")).Run(conn)
 		logs.Info("Set macvlan device into container failed", err)
 		self.delVLan(vethName)
 		return
 	}
 
-	cmd = exec.Command("nsenter", "-t", pid, "-n", "ip", "addr", "add", ips, "dev", vethName)
+	pid := strconv.Itoa(container.State.Pid)
+	cmd := exec.Command("nsenter", "-t", pid, "-n", "ip", "addr", "add", ips, "dev", vethName)
 	if err := cmd.Run(); err != nil {
 		gore.NewCommand("LPUSH", feedKey, fmt.Sprintf("0|||")).Run(conn)
 		logs.Info("Bind ip in container failed", err)
