76a2fcea8ec1b8c39cd858cae46262ec7b33e0f9
CMGS <ilskdw@gmail.com>
2015-08-03 16:46:28 +0800

    抄了 golang 源码山寨个可用的 shortfile

--- logs/logs.go
+++ logs/logs.go
@@ -1,7 +1,10 @@
 package logs
 
 import (
+	"fmt"
 	"log"
+	"runtime"
+	"strings"
 )
 
 var Mode bool = false
@@ -11,17 +14,34 @@ func init() {
 }
 
 func Info(v ...interface{}) {
+	f := getShortFile()
+	v = append([]interface{}{f}, v...)
 	log.Println(v...)
 }
 
 func Debug(v ...interface{}) {
 	if Mode {
+		f := getShortFile()
+		v = append([]interface{}{f}, v...)
 		log.Println(v...)
 	}
 }
 
 func Assert(err error, context string) {
 	if err != nil {
-		log.Fatal(context+": ", err)
+		f := getShortFile()
+		log.Fatal(f, context+": ", err)
 	}
+}
+
+func getShortFile() string {
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		file = "/???/???"
+		line = 0
+	}
+	s := strings.Split(file, "/")
+	l := len(s)
+	f := fmt.Sprintf("%s.%s:%d", s[l-2], s[l-1], line)
+	return f
 }
