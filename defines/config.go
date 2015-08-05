0b66e2171ac4e7e9f37fa80c32facdaf50b78d41
dengqian <dengqian@foxmail.com>
2015-08-05 17:41:48 +0800

    add config for calico

--- defines/config.go
+++ defines/config.go
@@ -45,6 +45,11 @@ type LimitConfig struct {
 	Memory uint64
 }
 
+type CalicoConfig struct {
+	NodeIP    string `yaml:"node_ip"`
+	NodeImage string `yaml:"node_image"`
+}
+
 type AgentConfig struct {
 	HostName string `yaml:"hostname"`
 	PidFile  string
@@ -57,4 +62,5 @@ type AgentConfig struct {
 	Redis   RedisConfig
 	API     APIConfig
 	Limit   LimitConfig
+	Calico  CalicoConfig
 }
