66fa8ed9fd3f835e7ee9c8888670cf666a063fe2
CMGS <ilskdw@gmail.com>
2015-09-22 17:28:49 +0800

    refactor route logical

--- network/route_linux.go
+++ network/route_linux.go
@@ -11,59 +11,63 @@ import (
 )
 
 //add default route
-func addDefaultRoute(gateway string) (err error) {
+func addDefaultRoute(gateway string) error {
 
 	gwIP := net.ParseIP(gateway)
 	route := netlink.Route{Gw: gwIP}
 
-	err = netlink.RouteAdd(&route)
-	if err != nil {
-		return err
-	}
-	return err
+	return netlink.RouteAdd(&route)
 }
 
-//delete default route
-func delDefaultRoute() (err error) {
+//delete default routes
+//FIXME all default routes will be erased
+func delDefaultRoute() error {
 	routes, _ := netlink.RouteList(nil, netlink.FAMILY_V4)
 
-	err = netlink.RouteDel(&routes[0])
-	if err != nil {
-		return err
+	for _, route := range routes {
+		if route.Dst != nil || route.Src != nil {
+			continue
+		}
+		if err := netlink.RouteDel(&route); err != nil {
+			return err
+		}
 	}
-	return err
+	return nil
 }
 
-func setDefaultRoute(cpid int, gateway string) (err error) {
+func setDefaultRoute(cid, gateway string, pid int) bool {
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 
 	origins, err := netns.Get()
 	if err != nil {
-		logs.Info("fail to get original namespace")
-		return err
+		logs.Info("Get orignal namespace failed", err)
+		return false
 	}
 	defer origins.Close()
 
-	ns, _ := netns.GetFromPid(cpid)
-	defer ns.Close()
+	ns, err := netns.GetFromPid(pid)
+	if err != nil {
+		logs.Info("Get container namespace failed", err)
+		return false
+	}
 
 	netns.Set(ns)
+	defer ns.Close()
 	defer netns.Set(origins)
 
-	err = delDefaultRoute()
-	if err != nil {
-		logs.Info("delete default routing table failed")
-		return err
+	if err := delDefaultRoute(); err != nil {
+		logs.Info("Delete default routing table failed", err)
+		return false
 	}
 
-	err = addDefaultRoute(gateway)
-	if err != nil {
-		logs.Info("add default routing table failed")
-		return err
+	if err := addDefaultRoute(gateway); err != nil {
+		logs.Info("add default routing table failed", err)
+		return false
 	}
 
-	return err
+	logs.Info("Set default route success", cid[:12], gateway)
+	return true
 }
 
 func SetDefaultRoute(cid, gateway string) bool {
@@ -80,12 +84,5 @@ func SetDefaultRoute(cid, gateway string) bool {
 
 	pid := container.State.Pid
 
-	err = setDefaultRoute(pid, gateway)
-	if err != nil {
-		logs.Info("set default route failed", err)
-		return false
-	}
-
-	logs.Info("Set default route success", cid[:12], gateway)
-	return true
+	return setDefaultRoute(cid, gateway, pid)
 }
