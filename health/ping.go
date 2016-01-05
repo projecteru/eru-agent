0c19de4494a0632eed14e67694be78c1a080fe88
tonic <tonicbupt@gmail.com>
2016-01-05 17:16:26 +0800

    add slash to url

--- health/ping.go
+++ health/ping.go
@@ -17,7 +17,7 @@ func ping() {
 	ticker := time.Tick(time.Duration(g.Config.Docker.Health) * time.Second)
 	for _ = range ticker {
 		if err := g.Docker.Ping(); err != nil {
-			url := fmt.Sprintf("%s/api/host/%s/down", g.Config.Eru.Endpoint, g.Config.HostName)
+			url := fmt.Sprintf("%s/api/host/%s/down/", g.Config.Eru.Endpoint, g.Config.HostName)
 			utils.DoPut(url)
 			logs.Assert(err, "Docker exit")
 		}
